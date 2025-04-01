a71ef5dbd7d436ce710e1873e8031c74ad360cf5
Alexandre Luiz <xandysilvaals@gmail.com>
2025-04-01 09:39:03 -0300

    Adicionando lista d e verduras no arquivo mensagem.py

--- mensagem.py
+++ mensagem.py
@@ -1,1 +1,6 @@
 mensagem = "Hello World! Utilizando Git no VSCode"
+
+
+verduras = ['alface', 'tomate', 'cenoura', 'beterraba', 'brócolis']
+for i in range(len(verduras)):
+    print(f'{i}: {verduras[i]}')
